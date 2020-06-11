80c0edcb21fb9b9fac0b1682b0fe6e05ca1a2e52
papercatnumber13 <66756754+papercatnumber13@users.noreply.github.com>
2020-06-10 20:06:04 -0400

    Update GuildNotif.py

--- CatBot/cogs/GuildNotif.py
+++ CatBot/cogs/GuildNotif.py
@@ -10,6 +10,16 @@ class GuildNotif(commands.Cog):
         channel = member.guild.system_channel
         if channel is not None:
             await channel.send('Welcome {0.mention}!'.format(member))
+            
+    @commands.command()
+    async def SafetyCheck(self,ctx):
+        answer = random.randint(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25)
+        if answer == 1:
+            answer = 'DANGER'
+        else:
+            answer = 'Safe'
+        await ctx.send(answer)
+    
 
 def setup(bot):
     bot.add_cog(GuildNotif(bot))
